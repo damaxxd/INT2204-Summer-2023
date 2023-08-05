58d8b9739849a391b135a98910f07bd08e67daad
vinh6203 <100813986+vinh6203@users.noreply.github.com>
2023-08-05 20:03:53 +0700

    fixing path in image resize file

--- btl/src/resources/icon/imageResize.py
+++ btl/src/resources/icon/imageResize.py
@@ -2,7 +2,7 @@ from PIL import Image
  
 ImgName = "download.png"
 savedImgName = "history_icon.png"
-path = "C:/Users/admin/Works/jva/oop/INT2204-Summer-2023/btl/src/resources/icon/" + ImgName
+path = "btl/src/resources/icon/" + ImgName
 im = Image.open(path)
 
 newsize = (30, 30)
